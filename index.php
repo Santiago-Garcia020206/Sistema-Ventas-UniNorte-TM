ef411802de718376ff702a8600fd1c85d319d744
Santiago-Garcia <leicity@gmail.com>
2026-09-03 11:12:40 -0300

    Added Template.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido a mi sitio web</h1>
+    <h2>Este es un subtítulo</h2>
+    <h3>Este es un tercer nivel de encabezado</h3>
+    <ul>
+        <li>Este es un elemento de lista</li>
+    </ul>
+    <div>
+        <p>Este es un párrafo dentro de un div.</p>
+        
+    </div>
+</body>
+</html>
